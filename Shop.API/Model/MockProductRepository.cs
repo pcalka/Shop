11643fa9a8cdb3d6b9ae4c80088ebe85e7859613
Przemysław Całka <przemekcalka1@outlook.com>
2020-04-15 05:16:10 +0200

    Add folder model and controller

---
+++ Shop.API/Model/MockProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.API.Model
+{
+    public class MockProductRepository
+    {
+        private List<Product> _productsList;
+        public MockProductRepository()
+        {
+
+        }
+
+        private void LoadProduct()
+        {
+            _productsList = new List<Product>
+                {
+                new Product { Id = 1, Name = "Ruler", Price = 1 },
+                new Product { Id = 3, Name = "Pencilecase", Price = 2 },
+                new Product { Id = 2, Name = "Pen", Price = 1 }
+                };
+        }
+    }   
+}

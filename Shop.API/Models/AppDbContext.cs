01a0178552f751fefc4bcbe95888492c60d4fd40
Przemysław Całka <przemekcalka1@outlook.com>
2020-04-24 22:11:22 +0200

    Add ef core, prepare to create database

---
+++ Shop.API/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shop.API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        { }
+        DbSet<Product> Products { get; set; }
+    }
+}

cea9016580f1d53291aa00953286566c3bd0bbfd
Przemysław Całka <przemekcalka1@outlook.com>
2020-04-25 19:02:32 +0200

    Add dbinitializer for product

---
+++ Shop.API/Models/DbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.API.Models
+{
+    public class DbInitializer
+    {
+        public void Seed(AppDbContext context)
+        {
+            if (context.Products == null)
+            {
+                context.AddRange(
+                        new Product { Id = 1, Name = "Ruler", Price = 1 },
+                        new Product { Id = 2, Name = "Pencilecase", Price = 2 },
+                        new Product { Id = 3, Name = "Pen", Price = 1 },
+                        new Product { Id = 4, Name = "Book", Price = 10 },
+                        new Product { Id = 5, Name = "Box", Price = 5 }
+                    );
+            }
+            context.SaveChanges();
+        }
+       
+    }
+}

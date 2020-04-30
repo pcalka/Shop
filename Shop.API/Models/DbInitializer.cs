30de3ece691f42352522e551c7455754849c591a
Przemysław Całka <przemekcalka1@outlook.com>
2020-04-30 05:27:48 +0200

    add two view for product controller and remove mock repository

--- Shop.API/Models/DbInitializer.cs
+++ Shop.API/Models/DbInitializer.cs
@@ -1,21 +1,22 @@
-﻿namespace Shop.API.Models
+﻿using System.Linq;
+
+namespace Shop.API.Models
 {
-    public class DbInitializer
+    public static class DbInitializer
     {
         public static void Seed(AppDbContext context)
         {
-            if (context.Products == null)
+            if (!context.Products.Any())
             {
                 context.AddRange(
-                        new Product { Id = 1, Name = "Ruler", Price = 1 },
-                        new Product { Id = 2, Name = "Pencilecase", Price = 2 },
-                        new Product { Id = 3, Name = "Pen", Price = 1 },
-                        new Product { Id = 4, Name = "Book", Price = 10 },
-                        new Product { Id = 5, Name = "Box", Price = 5 }
+                        new Product { Name = "Ruler", Price = 1 },
+                        new Product { Name = "Pencilecase", Price = 2 },
+                        new Product { Name = "Pen", Price = 1 },
+                        new Product { Name = "Book", Price = 10 },
+                        new Product { Name = "Box", Price = 5 }
                     );
             }
             context.SaveChanges();
-        }
-       
+        }       
     }
 }

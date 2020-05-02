3c92d48c09f0c6ee3667fd8f0353eb96bf1b3be7
Przemysław Całka <przemekcalka1@outlook.com>
2020-05-02 12:45:17 +0200

    Create admin controller

---
+++ Shop.API/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shop.API.Models;
+
+namespace Shop.API.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<User> _userManager { get; set; }
+
+        public AdminController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Shop.API/Models/User.cs
+++ Shop.API/Models/User.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shop.API.Models
 {
     public class User : IdentityUser
     {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", 
+            ErrorMessage = "E-mail is not valid")]
+        public override string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
     }
 }

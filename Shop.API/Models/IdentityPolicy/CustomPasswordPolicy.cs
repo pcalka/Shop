ca287a2800b9f7aeafeb027fe6cbb7e6738a9c99
Przemysław Całka <przemekcalka1@outlook.com>
2020-05-14 05:34:35 +0200

    Add folder Identity policy and create password policy

---
+++ Shop.API/Models/IdentityPolicy/CustomPasswordPolicy.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.API.Models.IdentityPolicy
+{
+    public class CustomPasswordPolicy : PasswordValidator<AppUser>
+    {
+        public override async Task<IdentityResult> ValidateAsync(UserManager<AppUser> userManager,
+            AppUser user, string password)
+        {
+            IdentityResult result = await base.ValidateAsync(userManager, user, password);
+            List<IdentityError> errors = result.Succeeded ? 
+                new List<IdentityError>() : result.Errors.ToList();
+            if (password.ToLower().Contains(user.UserName.ToLower()))
+                { 
+                    errors.Add(new IdentityError
+                    {
+                        Description = "Password can't contain username"
+                    });
+                }
+            if(password.Contains("123"))
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Password cannot contain 123 numeric sequence"
+                });
+            }
+            return errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray());
+        }
+    }
+}

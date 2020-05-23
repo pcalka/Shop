d6939e33d9640c696c114b2eb16180f7d7d5b5c3
Przemysław Całka <przemekcalka1@outlook.com>
2020-05-23 18:31:51 +0200

    Add Role controller with create method.

---
+++ Shop.API/Controllers/RoleController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.API.Controllers
+{
+    public class RoleController : Controller
+    {
+        private RoleManager<IdentityRole> _roleManager;
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public ViewResult Index() => View(_roleManager.Roles);
+
+        public IActionResult Crate() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            return View(name);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }
+}

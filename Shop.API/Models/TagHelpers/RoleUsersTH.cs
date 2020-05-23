10ba2e1da20b63ea239a3ea365c2782193078cfc
Przemysław Całka <przemekcalka1@outlook.com>
2020-05-24 01:14:52 +0200

    Create tag helpre for role user.

---
+++ Shop.API/Models/TagHelpers/RoleUsersTH.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.API.Models.TagHelpers
+{
+    [HtmlTargetElement("td", Attributes ="i-role")]
+    public class RoleUsersTH : TagHelper
+    {
+        private UserManager<AppUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        public RoleUsersTH(UserManager<AppUser> userManager,
+         RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HtmlAttributeName("i-role")]
+        public string Role { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context,
+            TagHelperOutput output)
+        {
+            List<string> Names = new List<string>();
+            IdentityRole RoleResult = await _roleManager.FindByIdAsync(Role);
+            if(Role != null)
+            {
+                foreach(var user in _userManager.Users)
+                {
+                    if (user != null && await _userManager.IsInRoleAsync(user, RoleResult.Name))
+                        Names.Add(user.UserName);
+                }
+            }
+            output.Content.SetContent(Names.Count == 0 ? "No users" : 
+                string.Join(",", Names));
+        }
+    }
+}

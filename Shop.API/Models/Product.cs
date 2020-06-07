eeeb4883ffc946c3e1062ea8d376af465971d89f
Przemysław Całka <przemekcalka1@outlook.com>
2020-06-07 21:30:06 +0200

    Change details view in product.

--- Shop.API/Models/Product.cs
+++ Shop.API/Models/Product.cs
@@ -1,9 +1,13 @@
-﻿namespace Shop.API.Models
+﻿using System.Collections.Generic;
+
+namespace Shop.API.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; }        
+        public string Descrption { get; set; }
         public float Price { get; set; }
+        public List<string> PhotoUrl { get; set; }
     }
 }

c20c808e3a09538dbbb2c8fe0a76e7cec083d68a
Przemysław Całka <przemekcalka1@outlook.com>
2020-06-09 18:42:01 +0200

    Add migration for favourite.

---
+++ Shop.API/Migrations/20200609163917_AddFavouriteForProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.API.Migrations
+{
+    public partial class AddFavouriteForProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavourite",
+                table: "Products",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFavourite",
+                table: "Products");
+        }
+    }
+}
